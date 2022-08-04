ff38ff522147eeefb2c7c407fae5eaa01b56d42f
oleksandr.shevchuk222@gmail.com <oleksandr.shevchuk222@gmail.com>
2022-08-04 13:20:27 +0300

    Add project files.

---
+++ MyQueue/Model/Item.cs
@@ -0,0 +1,16 @@
+﻿namespace MyQueue.Model
+{
+    public class Item<T>
+    {
+        public T Data { get; set; } 
+        public Item<T> Next { get; set; }
+        public Item(T data)
+        {
+            Data = data;
+        }
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ MyQueue/Program.cs
@@ -0,0 +1,89 @@
+﻿using MyQueue.Model;
+using System;
+
+namespace MyQueue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var duplexLinkedDeque = new DuplexLinkedDeque<int>();
+            duplexLinkedDeque.PushFront(1);
+            duplexLinkedDeque.PushFront(2);
+            duplexLinkedDeque.PushFront(3);
+            duplexLinkedDeque.PushBack(4);
+            duplexLinkedDeque.PushBack(5);
+            duplexLinkedDeque.PushFront(6);
+            duplexLinkedDeque.PushBack(7);
+
+            Console.WriteLine(duplexLinkedDeque.PopFront());
+            Console.WriteLine(duplexLinkedDeque.PopFront());
+            Console.WriteLine(duplexLinkedDeque.PopBack());
+            Console.WriteLine(duplexLinkedDeque.PopBack());
+            Console.WriteLine(duplexLinkedDeque.PopFront());
+
+
+            Console.WriteLine();
+
+            var easyDeque = new EasyDeque<int>();
+            easyDeque.PushFront(1);
+            easyDeque.PushFront(2);
+            easyDeque.PushFront(3);
+            easyDeque.PushBack(40);
+            easyDeque.PushBack(50);
+            easyDeque.PushFront(600);
+            easyDeque.PushBack(700);
+
+            Console.WriteLine(easyDeque.PopFront());
+            Console.WriteLine(easyDeque.PopBack());
+            Console.WriteLine(easyDeque.PopFront());
+            Console.WriteLine(easyDeque.PopBack());
+
+
+            Console.WriteLine();
+
+
+            var linkedQueue = new LinkedQueue<int>();
+            linkedQueue.Enqueue(1);
+            linkedQueue.Enqueue(2);
+            linkedQueue.Enqueue(3);
+            linkedQueue.Enqueue(4);
+            linkedQueue.Enqueue(5);
+
+            Console.WriteLine(linkedQueue.Dequeue());
+            Console.WriteLine(linkedQueue.Peek());
+            Console.WriteLine(linkedQueue.Dequeue());
+            Console.WriteLine(linkedQueue.Dequeue());
+            Console.WriteLine(linkedQueue.Dequeue());
+            Console.WriteLine(linkedQueue.Dequeue());
+
+            Console.WriteLine();
+
+            var arrayQueue = new ArrayQueue<int>(10);
+            arrayQueue.Enqueue(1);
+            arrayQueue.Enqueue(2);
+            arrayQueue.Enqueue(3);
+            arrayQueue.Enqueue(4);
+            arrayQueue.Enqueue(5);
+
+            Console.WriteLine(arrayQueue.Dequeue());
+            Console.WriteLine(arrayQueue.Peek());
+            Console.WriteLine(arrayQueue.Dequeue());
+            Console.WriteLine(arrayQueue.Dequeue());
+            Console.WriteLine(arrayQueue.Dequeue());
+            Console.WriteLine(arrayQueue.Dequeue());
+
+            Console.WriteLine();
+            var easyQueue = new EasyQueue<int>();
+            easyQueue.Enqueue(1);
+            easyQueue.Enqueue(2);
+            easyQueue.Enqueue(3);
+            easyQueue.Enqueue(4);
+            easyQueue.Enqueue(5);
+
+            Console.WriteLine(easyQueue.Dequeue());
+            Console.WriteLine(easyQueue.Peek());
+            Console.WriteLine(easyQueue.Dequeue());
+        }
+    }
+}
